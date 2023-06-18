e2cd1c21c1a77a319e69ac4bdb3c1de0b830bf0e
nicho-UJN <nicholas9698@outlook.com>
2023-06-18 17:49:30 +0800

    add tokenizer builder

--- models/tokenizer/tokenizer_building.py
+++ models/tokenizer/tokenizer_building.py
@@ -1,6 +1,10 @@
 import os
-from tokenizers import ByteLevelBPETokenizer
-
+from tokenizers import Tokenizer
+from tokenizers.models import WordPiece
+from tokenizers.normalizers import BertNormalizer
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.processors import TemplateProcessing
+from tokenizers.trainers import WordPieceTrainer
 
 data_path = '../../data/tokenizer/'
 paths = []
@@ -10,10 +14,33 @@ for file in filenames:
         paths.append(data_path+file)
 
 # initialize
-tokenizer = ByteLevelBPETokenizer()
+tokenizer = Tokenizer(WordPiece(unk_token="[UNK]"))
+
+# normalize
+tokenizer.normalizer = BertNormalizer(clean_text=True, handle_chinese_chars=True, lowercase=True)
+
+# split by whitespace
+pre_tokenizer = Whitespace()
+tokenizer.pre_tokenizer = pre_tokenizer
+
+# processing way to single or pair
+tokenizer.post_processor = TemplateProcessing(
+    single="[CLS] $A [SEP]",
+    pair="[CLS] $A [SEP] $B:1 [SEP]:1",
+    special_tokens=[
+        ("[CLS]", 1),
+        ("[SEP]", 2)
+    ]
+)
+
+# Define WordPieceTrainer
+trainer = WordPieceTrainer(
+    vocab_size=100,
+    special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"]
+)
 
 # training
-tokenizer.train(files=paths, vocab_size=30_522, min_frequency=2, special_tokens=['<s>', '<pad>', '</s>', '<unk>', '<mask>']) 
+tokenizer.train(files=paths, trainer=trainer) 
 
 if not os.path.exists('residue'):
     os.mkdir('residue')
