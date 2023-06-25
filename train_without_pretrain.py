e1f3c8e6fe63b7194ce93b016e46571f29c3fb17
nicho-UJN <nicholas9698@outlook.com>
2023-06-25 17:18:53 +0800

    add training process

--- train_without_pretrain.py
+++ train_without_pretrain.py
@@ -1,8 +1,12 @@
+import sys
 import torch
+import time
 import random
 import numpy as np
+from utils.tools import time_since
 from torch.optim import AdamW
 from transformers import BertTokenizer, get_linear_schedule_with_warmup
+from utils.pre_data import load_data_target, prepare_train_batch, pad_sequence
 from models.ResidueEncoderDecoderModel import ResidueEncoderDecoderModel
 
 USE_CUDA = torch.cuda.is_available()
@@ -13,6 +17,8 @@ learning_rate = 5e-5
 weight_decay = 1e-5
 n_epoch = 80
 seed = 42
+train_file = 'data/allosteric_site/data_train.json'
+test_file = 'data/allosteric_site/data_test.json'
 
 def set_seed(seed:int):
     random.seed(seed)
@@ -22,8 +28,12 @@ def set_seed(seed:int):
 
 set_seed(seed)
 
-model = ResidueEncoderDecoderModel.from_encoder_decoder_pretrained('models/residue-roberta', 'models/residue-roberta', tie_encoder_decoder=True, encoder_config='models/residue-roberta/config.json')
+model = ResidueEncoderDecoderModel.from_encoder_decoder_pretrained('models/residue-roberta', 'models/residue-roberta', tie_encoder_decoder=True)
 tokenizer = BertTokenizer.from_pretrained('models/residue-roberta')
+new_tokens = ['0', '1']
+tokenizer.add_tokens(new_tokens)
+model.encoder.resize_token_embeddings(len(tokenizer))
+model.decoder.resize_token_embeddings(len(tokenizer))
 
 # set model's config
 model.config.decoder_start_token_id = tokenizer.cls_token_id
@@ -40,4 +50,40 @@ for n, p in model.named_parameters():
     size += p.nelement()
 print('Total parameters: {}'.format(size))
 
-# model.save_pretrained('test/')
+train_pair = load_data_target(train_file, tokenizer)
+test_pair = load_data_target(test_file, tokenizer)
+
+if USE_CUDA:
+    model.cuda()
+
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_ground_paramters = [
+    {'params': [p for n,p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+    {'params': [p for n,p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+optimizer = AdamW(optimizer_ground_paramters, lr=learning_rate, eps=1e-8)
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0.1*n_epoch, num_training_steps=n_epoch)
+
+model.zero_grad()
+
+total = 0 
+
+for epoch in range(n_epoch):
+    loss_total = 0
+    print('epoch', epoch+1)
+    data_batches, target_batches = prepare_train_batch(train_pair, batch_size)
+    start_time = time.time()
+    model.train()
+    for idx in range(len(data_batches)):
+        inputs = pad_sequence(data_batches[idx], target_batches[idx], tokenizer, USE_CUDA)
+        loss = model(**inputs).loss
+        loss.backward()
+        loss_total += loss
+
+        optimizer.step()
+        model.zero_grad()
+
+    print("loss:", loss_total / len(data_batches))
+    print("training time", time_since(time.time() - start_time))
+    print("--------------------------------")
+    scheduler.step()
---
+++ utils/tools.py
@@ -0,0 +1,9 @@
+import math
+
+# compute time
+def time_since(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    h = math.floor(m / 60)
+    m -= h * 60
+    return '%dh %dm %ds' % (h, m, s)
